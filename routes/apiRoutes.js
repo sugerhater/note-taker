a3c740892230d9bf3844aa67a3e0657c420aeed4
sugerhater <kehong0926@gmail.com>
2021-01-11 12:56:54 -0500

    updated app.post('/api/notes')

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile)
+let allNotes;
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     readFileAsync('../data/db.json').then(function(data){
@@ -11,7 +12,17 @@ module.exports = function(app) {
   });
 
   app.post("/api/notes", function(req, res) {
-  
+      let newNote = req.body;
+      readFileAsync('../data/db.json').then(function(data){
+        allNotes = JSON.parse(data)
+      })
+      let newNoteId = allNotes.length +1;
+      newNote['id'] = newNoteId;
+      allNotes.push(newNote);
+      writeFileAsync('../data/db.json',allNotes).then(function(){
+        res.json(newNote);
+      })
+      
   });
 
   app.delete("/api/notes/:id", function(req, res) {
