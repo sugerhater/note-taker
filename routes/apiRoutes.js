ee3d585961364a1bb22554716999f4f9166d1343
sugerhater <kehong0926@gmail.com>
2021-01-11 12:27:37 -0500

    set up route:/api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,13 @@
 var noteData = require("../data/db.json");
-
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile)
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
-    res.json(noteData);
+    readFileAsync('../data/db.json').then(function(data){
+      res.json(data)
+    })
   });
 
   app.post("/api/notes", function(req, res) {
