b96c42c1f9dc0a74bc947d5e0ca44374766278d3
sugerhater <kehong0926@gmail.com>
2021-01-12 12:39:28 -0500

    setup app.delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,46 +4,63 @@ const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile)
 let allNotes;
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/notes", function(req, res) {
-    readFileAsync('../db/db.json').then(function(data){
-      try{
+  app.get("/api/notes", function (req, res) {
+    readFileAsync('../db/db.json').then(function (data) {
+      try {
         res.json(data)
 
-      } catch(err){
+      } catch (err) {
         console.log(err)
       }
     })
   });
 
-  app.post("/api/notes", function(req, res) {
-    try{
+  app.post("/api/notes", function (req, res) {
+    try {
 
       let newNote = req.body;
-      readFileAsync('../db/db.json').then(function(data){
+      readFileAsync('../db/db.json').then(function (data) {
         allNotes = JSON.parse(data)
       })
-      
+
       // This means you'll need to find a way to give each note a unique `id` when it's saved.
       //recreate id;
-      let newNoteId = allNotes.length + 1;
+      // let newNoteId = allNotes.length + 1;
+      //set the new id = 0 if json file is empty, if not empty, new id = id of the last item +1;
+      let newNoteId = 0;
+
+      if (allNotes.length === 0) {
+
+      } else {
+        newNoteId = allNotes[allNotes.length - 1].id + 1;
+      }
       newNote['id'] = newNoteId;
       allNotes.push(newNote);
-      writeFileAsync('../db/db.json',allNotes).then(function(){
+      writeFileAsync('../db/db.json', allNotes).then(function () {
         res.json(newNote);
       })
-    } catch(err){
+    } catch (err) {
       console.log(err)
     }
-      
+
   });
 
   // when someone comes to this route w/ a delete req
-  app.delete("/api/notes/:id", function(req, res) {
+  app.delete("/api/notes/:id", function (req, res) {
     try {
-
-    }catch(err){
+      let deleteId = req.params.id;
+      readFileAsync('../db/db.json').then(function (data) {
+        allNotes = JSON.parse(data)
+      });
+      allNotes = allNotes.filter(note=>{
+        return note.id != deleteId;
+      })
+      writeFileAsync('../db/db.json',allNotes).then(function(){
+        res.json(allNotes)
+      })
+    } catch (err) {
       console.log(err)
     }
     // Should receive a query parameter containing the id of a note to delete.
